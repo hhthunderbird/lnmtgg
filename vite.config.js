a46dd7f1c1149536e24d22e7fd9a1f4174b28877
ThunderTrabuco <hhthunderbird@gmail.com>
2025-05-01 13:35:20 -0300

    fix settings

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import path from 'path' // Add this import
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
-    base: '/',
     plugins: [react()],
     build: {
         // Add these options for better Vercel compatibility
         outDir: 'dist',
         emptyOutDir: true,
+        rollupOptions: {
+          input: {
+            main: path.resolve(__dirname, 'index.html') // Explicit entry point
+          }
+        }
     }
 });
