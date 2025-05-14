9b00920248535748e57ed3225e966fb18f799b26
ThunderTrabuco <hhthunderbird@gmail.com>
2025-05-14 11:59:36 -0300

    fix: resolve remaining TypeScript errors - Remove unused Suspense import from App.tsx - Remove unused styled components from JsonFormatter.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import HomePage from './components/HomePage';
